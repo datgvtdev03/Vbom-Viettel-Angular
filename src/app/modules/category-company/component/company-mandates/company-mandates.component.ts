84380ce96db44b6c27bb6be47f19aa48939b84d8
makodu1mat <makodu1mat@gmail.com>
2023-08-17 09:49:59 +0700

    17-08 update fix name category-company

---
+++ src/app/modules/category-company/component/company-mandates/company-mandates.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vbom-company-mandates',
+  templateUrl: './company-mandates.component.html',
+  styleUrls: ['./company-mandates.component.scss']
+})
+export class CompanyMandatesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
